082f440313ec332b290e2793960325adae81084f
Padam7890 <padamthapamoho@gmail.com>
2024-09-18 20:26:12 +0545

    swagger fix

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
     }, "JWT-auth")
     .build();
   const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/api/docs', app, document);
   await app.listen(3001);
 }
 bootstrap();
