ee4efe02fabafdc9ee4d6990ef04343a6c4b8af9
Padam7890 <padamthapamoho@gmail.com>
2024-09-19 16:02:54 +0545

    second hero section

--- src/second-hero-section/second-hero-section.controller.ts
+++ src/second-hero-section/second-hero-section.controller.ts
@@ -12,6 +12,10 @@ import { SecondHeroSectionService } from './second-hero-section.service';
 import { CreateSecondHeroSectionDto } from './dto/create-second-hero-section.dto';
 import { UpdateSecondHeroSectionDto } from './dto/update-second-hero-section.dto';
 import { createResponse } from 'src/helper/response.helper';
+import { UniversalDecorator } from 'src/common/decorators/universal.decorator';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+
 
 @Controller('second-hero-section')
 export class SecondHeroSectionController {
@@ -20,28 +24,56 @@ export class SecondHeroSectionController {
   ) {}
 
   @Post()
+  @UniversalDecorator({
+    guards: [AuthGuard, RoleGuard],
+    role: 'USER',
+    summary: 'Form Second Hero Section',
+    responseType: CreateSecondHeroSectionDto,
+    includeBearerAuth: true,
+  })
+
   async create(@Body() createSecondHeroSectionDto: CreateSecondHeroSectionDto) {
     const secondHeroSection = await this.secondHeroSectionService.create(
       createSecondHeroSectionDto,
     );
     return createResponse(
       HttpStatus.CREATED,
-     'second hero section created successfully',
+      'second hero section created successfully',
       secondHeroSection,
     );
   }
 
   @Get()
-  findAll() {
-    return this.secondHeroSectionService.findAll();
+  @UniversalDecorator({
+    summary: 'All Hero Section Content',
+    responseType: CreateSecondHeroSectionDto,
+  })
+  async findAll() {
+    const secondHeroSection = await this.secondHeroSectionService.findAll();
+    return createResponse(
+      HttpStatus.OK,
+      'second hero section fetched successfully',
+      secondHeroSection,
+    );
   }
 
   @Get(':id')
+  @UniversalDecorator({
+    summary: 'One Hero Section Content',
+    responseType: CreateSecondHeroSectionDto,
+  })
   findOne(@Param('id') id: string) {
     return this.secondHeroSectionService.findOne(+id);
   }
 
   @Patch(':id')
+  @UniversalDecorator({
+    guards: [AuthGuard, RoleGuard],
+    role: 'USER',
+    summary: 'Update Form Second Hero Section',
+    responseType: CreateSecondHeroSectionDto,
+    includeBearerAuth: true,
+  })
   update(
     @Param('id') id: string,
     @Body() updateSecondHeroSectionDto: UpdateSecondHeroSectionDto,
@@ -53,6 +85,13 @@ export class SecondHeroSectionController {
   }
 
   @Delete(':id')
+  @UniversalDecorator({
+    guards: [AuthGuard, RoleGuard],
+    role: 'USER',
+    summary: ' Second Hero Section Delete',
+    responseType: CreateSecondHeroSectionDto,
+    includeBearerAuth: true,
+  })
   remove(@Param('id') id: string) {
     return this.secondHeroSectionService.remove(+id);
   }
--- src/second-hero-section/second-hero-section.module.ts
+++ src/second-hero-section/second-hero-section.module.ts
@@ -3,10 +3,12 @@ import { SecondHeroSectionService } from './second-hero-section.service';
 import { SecondHeroSectionController } from './second-hero-section.controller';
 import { DatabaseService } from 'src/database/database.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SecondHeroSectionController],
   providers: [SecondHeroSectionService],
-  imports:[DatabaseModule]
+  imports:[DatabaseModule, UserModule, AuthModule]
 })
 export class SecondHeroSectionModule {}
