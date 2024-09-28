1aa4578dd4ee70820f76a3a9a331082118975946
Padam7890 <padamthapamoho@gmail.com>
2024-09-28 19:28:52 +0545

    alternation section create method saved

--- src/domain/alternating-section/alternating-section.controller.ts
+++ src/domain/alternating-section/alternating-section.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
 import { AlternatingSectionService } from './alternating-section.service';
 import { CreateAlternatingSectionDto } from './dto/create-alternating-section.dto';
 import { UpdateAlternatingSectionDto } from './dto/update-alternating-section.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { UniversalDecorator } from 'src/common/decorators/universal.decorator';
+import { createResponse } from 'src/helper/response.helper';
 
 @Controller('alternating-section')
+@ApiTags("alternating-section")
 export class AlternatingSectionController {
   constructor(private readonly alternatingSectionService: AlternatingSectionService) {}
 
   @Post()
-  create(@Body() createAlternatingSectionDto: CreateAlternatingSectionDto) {
-    return this.alternatingSectionService.create(createAlternatingSectionDto);
+  @UniversalDecorator({
+    role: "admin",
+    summary: "Create a new alternating section",
+    responseType: CreateAlternatingSectionDto,
+    includeBearerAuth: true,
+  })
+  
+ async  create(@Body() createAlternatingSectionDto: CreateAlternatingSectionDto) {
+    const alternatingSection = await this.alternatingSectionService.create(createAlternatingSectionDto);
+    return createResponse(HttpStatus.CREATED, "Alternating section created successfully", alternatingSection);
   }
 
   @Get()
+  @UniversalDecorator({
+    summary: "Get all alternating sections",
+    responseType: CreateAlternatingSectionDto,
+  })
   findAll() {
     return this.alternatingSectionService.findAll();
   }
